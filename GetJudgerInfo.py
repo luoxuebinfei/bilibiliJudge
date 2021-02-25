609dcf02202a31d5ca55945ba5d182fdb4d5f503
luoxuebinfei <39854534+luoxuebinfei@users.noreply.github.com>
2021-02-25 13:17:30 +0800

    Update GetJudgerInfo.py

--- GetJudgerInfo.py
+++ GetJudgerInfo.py
@@ -1,5 +1,6 @@
 import requests as r
 import json as js
+from fake_useragent import UserAgent
 
 url='http://api.bilibili.com/x/credit/jury/jury'
 
@@ -7,6 +8,8 @@ def GetInfo(SESSDATA):
     test='SESSDATA={}'.format(SESSDATA)
     headers={
         'cookie': test,
+        'Host': 'api.bilibili.com',
+        'User-Agent': UserAgent().random,
     }
     info=r.get(url,headers=headers)
     info_loads=js.loads(info.text)
@@ -14,11 +17,15 @@ def GetInfo(SESSDATA):
         1: '具有资格',
         2: '资格失效'
     }
+    #print(info_loads)
     parsed=str({
-        '用户名': info_loads['data']['uname'][0]+ ('*' * int(len(info_loads['data']['uname'])-2)) +info_loads['data']['uname'][len(info_loads['data']['uname'])-1:],
+        '用户名': info_loads['data']['uname'],
         '已裁决案件数': info_loads['data']['caseTotal'],
         '资格状态': status[info_loads['data']['status']],
         '剩余资格天数': info_loads['data']['restDays'],
         '裁决准确率': str(info_loads['data']['rightRadio'])+'%'
     })
+    
     return info_loads,parsed
+
+#GetInfo("581fcc29%2C1629713738%2C2f413%2A21")
